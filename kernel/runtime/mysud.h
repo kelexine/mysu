374e1263a1a3ecfcd6c0abafdbc5d3e9274a38df
kelexine <frankiekelechi@gmail.com>
2026-08-22 08:45:54 +0100

    fix(kernel): declare mysu_execve_hook_mysud in mysud.h
    Adds function prototypes for mysu_execve_hook_mysud,
    mysu_execveat_hook_mysud, mysu_handle_execveat_mysud, and
    mysu_stop_input_hook_runtime to resolve implicit declaration errors in
    syscall_event_bridge.c.
    
    Signed-off-by: kelexine <kelexine@proton.me>

--- kernel/runtime/mysud.h
+++ kernel/runtime/mysud.h
@@ -3,7 +3,15 @@
 
 #define MYSUD_PATH "/data/adb/mysud"
 
+struct pt_regs;
+struct user_arg_ptr;
+
 void mysu_mysud_init(void);
 void mysu_mysud_exit(void);
+
+void mysu_execve_hook_mysud(const struct pt_regs *regs);
+void mysu_execveat_hook_mysud(const struct pt_regs *regs);
+void mysu_handle_execveat_mysud(const char *path, struct user_arg_ptr *argv);
+void mysu_stop_input_hook_runtime(void);
 
 #endif
